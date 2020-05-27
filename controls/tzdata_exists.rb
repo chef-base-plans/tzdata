299cf6ecf2b4ec200cc6eb29b5b702a3c06387c7
Steven Marshall <SMarshall@chef.io>
2020-05-27 13:14:15 +0100

    [tzdata] Using HAB_ORIGIN from ENV
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/tzdata_exists.rb
+++ controls/tzdata_exists.rb
@@ -1,8 +1,8 @@
 plan_timezones = input("plan_timezones")
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input("plan_name", value: "tzdata")
 base_dir = input("base_dir", value: "include")
-
-plan_ident = "#{ENV["HAB_ORIGIN"]}/#{plan_name}"
+plan_ident = "#{plan_origin}/#{plan_name}"
 
 hab_pkg_path = command("hab pkg path #{plan_ident}")
 describe hab_pkg_path do
